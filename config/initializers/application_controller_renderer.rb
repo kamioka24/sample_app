253ca77937b6585b2993d6304b7eab9df996a7fc
上岡圭介 <kamioka_24@kyapunoMacBook-Pro.local>
2020-04-21 11:19:35 +0900

    [Finish] 3-10

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '9592f9f5d51b7ba7068196b82920bdabb8328d20b789e70b6351c7c958c430a4e2c35f4a85f45908d1edf2ac7a19e62bc52a477ff2973561f5035f671d0d9f66'
